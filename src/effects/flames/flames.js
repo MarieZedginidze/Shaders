c70d855d79df4ff0fdf20189fb9e06be1b2d7aa7
zach-planet-nine <zach@planetnine.app>
2024-05-27 23:40:31 -0700

    flames effect

---
+++ src/effects/flames/flames.js
@@ -0,0 +1,103 @@
+import { EntityComponentSystem } from 'javascript-entity-component-system';
+
+export const setAflame = () => {
+  const ecs = new EntityComponentSystem();
+
+  const canvas = document.getElementById('canvas2');
+  let context = canvas.getContext('2d');
+  const viewport = {width: 400, height: 500}; // this should be set based on the canvs.
+
+  const physicsComponent = {
+    name: "physics",
+    state: {
+      accelerationY: 1,
+      velocityX: 0,
+      velocityY: 1
+    }
+  };
+
+  const emojis = [`🔥`];
+
+  let positionComponents = [];
+  let emojiComponents = [];
+
+//  for(var i = 0; i < 40; i++) {
+    positionComponents.push({
+      name : "position", // + Math.floor(Math.random() * 1000),
+      componentType: 'position',
+      state: {
+	x: Math.floor(Math.random() * viewport.width),
+	y: (viewport.height - 250) - (Math.floor(Math.random() * 100))
+      }
+    });
+    emojiComponents.push({
+      name: 'text', // + Math.floor(Math.random() * 1000),
+      componentType: 'text',
+      state: {
+	  text: emojis[0],
+	  font: '40px Verdana'
+      }
+    });
+ // }
+
+  const physicsProcessor = {
+    name: "physicsProcessor",
+    required: ["position", "physics"],
+    update(entity, components, processor) {
+      const [position, physics] = components;
+
+      physics.state.velocityY += physics.state.accelerationY;
+      physics.state.velocityX *= (Math.random() < 0.5 ? -1 : 1);
+
+      position.state.y += physics.state.velocityY
+      position.state.x += physics.state.velocityX
+    }
+  };
+
+  const textProcessor = {
+    name: 'textProcessor',
+    required: ['position', 'text'],
+    update(entity, components, processor) {
+	const [position, text] = components;
+console.log('should print flame');
+
+	if(position.state.remove) {
+	    return;
+	}
+
+console.log('should really print flame at: ', position.state.x, position.state.y);
+console.log(text.state);
+console.log(context);
+	context.translate(position.state.x, position.state.y);
+	context.font = text.state.font;
+	context.fillStyle = 'black';
+	context.fillText(text.state.text, 0, 0);
+	context.translate(-position.state.x, -position.state.y)
+    }
+  };
+
+  positionComponents.forEach(component => ecs.addComponent(component));
+  ecs.addComponent(physicsComponent)
+
+  emojiComponents.forEach(component => ecs.addComponent(component));
+
+  ecs.addProcessor(physicsProcessor);
+  ecs.addProcessor(textProcessor);
+
+  const ent = ecs.createEntity('foo', [
+    'position',
+    'physics',
+    'text'
+  ], [
+    'physicsProcessor',
+    'textProcessor'
+  ]);
+  ecs.addEntity(ent);
+
+  setTimeout(() => {
+    console.log("should add some flames");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+//    context = canvas.getContext('2d');
+    ecs.update();
+  }, 100);
+};
---
+++ src/effects/flames/index.js
@@ -0,0 +1,93 @@
+import * as flames from './flames.js';
+
+const loadImage = () => {
+  window.onload = function () {
+        // get the image.
+        let img = new Image();
+        img.src = './image.jpg';
+
+        // wait till image is loaded.
+        img.onload = function () {
+//            fill_canvas(img);       // fill the canvas with the image.
+        }
+
+        function fill_canvas(img) {
+            let canvas = document.getElementById('canvas');
+            let ctx = canvas.getContext('2d'); // create canvas context.
+
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+            ctx.drawImage(img, 0, 0);       // draw the image to the canvas.
+
+            console.log(canvas.width);
+
+            console.log('set it alight');
+            flames.setAflame();
+        }
+    };
+};
+
+const positiveOrNegative = () => {
+  return Math.random() < 0.5 ? -1 : 1;
+};
+
+const randomDifference = (max) => {
+  return positiveOrNegative() * Math.floor(Math.random() * max);
+};
+
+const randomBrown = () => {
+  return {
+    red: 140 + randomDifference(15),
+    green: 80 + randomDifference(7),
+    blue: 52 + randomDifference(5)
+  };
+};
+
+const reddenItUp = () => {
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d'); // create canvas context.
+console.log("now canvas width is: " + canvas.width);
+console.log("canvas.height is: " + canvas.height);
+
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+console.log('length: ' + imageData.data.length);
+
+let foo = 0;
+  for(let i = ((canvas.height * canvas.width * 4) - (canvas.width * 4)); i > 0; i -= (canvas.width * 4)) {
+console.log("starting to redden");
+    foo++;
+    setTimeout(() => {
+//console.log("firing the redden");
+      for(let j = 0; j < (canvas.width * 4); j += 4) {
+        const rgb = randomBrown();
+	imageData.data[i + j] = rgb.red;
+        imageData.data[i + j + 1] = rgb.green;
+        imageData.data[i + j + 2] = rgb.blue;
+      }
+//console.log("should put image data");
+      ctx.putImageData(imageData, 0, 0);
+    }, 15 * foo);
+    setTimeout(() => {
+      for(let j = 0; j < (canvas.width * 4); j += 4) {
+        imageData.data[i + j + 3] = 150;
+      }
+    }, 25 * (foo + 1));
+    setTimeout(() => {
+      for(let j = 0; j < (canvas.width * 4); j += 4) {
+        imageData.data[i + j + 3] = 0;
+      }
+    }, 30 * (foo + 2));
+  }
+ 
+  console.log('fire!');
+  flames.setAflame();
+  
+};
+
+(() => {
+  loadImage();
+  setTimeout(reddenItUp, 5000);
+})();
+
+
